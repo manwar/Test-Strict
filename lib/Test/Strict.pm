cf00a7be3b196c95e96520454da7ebeaeb3d7c3c
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-10-19 18:02:49 +0100

    - Prepared for release to CPAN.

--- lib/Test/Strict.pm
+++ lib/Test/Strict.pm
@@ -68,7 +68,7 @@ use File::Find;
 use Config;
 
 our $COVER;
-our $VERSION = '0.28';
+our $VERSION = '0.29';
 our $PERL    = $^X || 'perl';
 our $COVERAGE_THRESHOLD = 50; # 50%
 our $UNTAINT_PATTERN    = qr|^(.*)$|;
